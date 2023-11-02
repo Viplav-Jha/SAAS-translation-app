29c0e37cbf2bca1d91e18c4fb7d6e0a8689d6ae1
Viplav jha <viplavjha1993@gmail.com>
2023-11-02 16:23:34 +0530

    code

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,28 +1,50 @@
 
-"use client"
+import { getServerSession } from "next-auth";
+import React, { useEffect, useState } from "react"; // Import useEffect and useState if needed
+import Logo from "./Logo";
+import DarkModeToggle from "./DarkModeToggle";
+import UserButton from "./UserButton";
+import { authOptions } from "@/auth";
+import Link from "next/link";
+import { MessageSquareIcon } from "lucide-react";
+import CreatChatButton from "./CreatChatButton";
 
-import React from 'react'
-import Logo from './Logo';
-import DarkModeToggle from './DarkModeToggle';
-import UserButton from './UserButton';
 
-const Header = () => {
-  return <header className='sticky top-0 z-50 dark:bg-gray-900'>
-    <nav className='flex flex-col sm:flex-row items-center p-5 pl-2 bg-white
-    dark:bg-gray-900 max-w-7xl mx-auto '>
-     <Logo/>
-     <div className='flex-1 flex items-center justify-end space-x-4'>
-      {/* LanguageSelect */}
-      {/* Session && (
-        ...
-      ) */}
-    <DarkModeToggle/>
-     <UserButton/>
-     </div>
-    </nav>
-    {/* Upgrade Banner */}
-  </header>
-  
+async function Header() {
+  const session = await getServerSession(authOptions);
+  console.log(session);
+
+  return (
+    <header className="sticky top-0 z-50 dark:bg-gray-900">
+      <nav
+        className="flex flex-col sm:flex-row items-center p-5 pl-2 bg-white
+        dark:bg-gray-900 max-w-7xl mx-auto"
+      >
+        {/* <Logo /> */}
+        
+        <div className="flex-1 flex items-center justify-end space-x-4">
+          {/* LanguageSelect */}
+            {session ?  (
+              <>
+                <Link href={'/chat'} prefetch={false}>
+                  <MessageSquareIcon
+                  className="text-black dark:text-white"
+                  />
+                  </Link>
+                  <CreatChatButton/>
+              </>
+            ):(
+             <Link href='/pricing'>
+              Pricing
+             </Link> 
+            )}
+          <DarkModeToggle />
+          <UserButton session={session} />
+        </div>
+      </nav>
+      {/* Upgrade Banner */}
+    </header>
+  );
 }
 
 export default Header;
