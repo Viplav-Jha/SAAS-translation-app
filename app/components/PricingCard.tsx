27b226c4f4fdb7ab933da49f6c493191ce1f7e10
Viplav jha <viplavjha1993@gmail.com>
2023-11-13 18:14:44 +0530

    pricingcard

--- app/components/PricingCard.tsx
+++ app/components/PricingCard.tsx
@@ -46,62 +46,60 @@ function PricingCard({ redirect }: { redirect: boolean }) {
             className="flex flex-col justify-between rounded-3xl bg-white p-8 shadow-xl
                    ring-1 ring-gray-900/10 sm:p-10"
           >
-           
-           
-              <h3
-                id={tier.id + tier.name}
-                className="text-base font-semibold leading-7 text-indigo-600"
-              >
-                {tier.name}
-              </h3>
-              <div className="mt-4">
-                {tier.priceMonthly ? (
-                  <>
-                    <span className="text-5xl font-bold tracking-tight text-gray-900">
-                      {tier.priceMonthly}
-                    </span>
-                    <span className="text-base font-semibold leading-7  text-gray-600">
-                      /month
-                    </span>
-                  </>
-                ) : (
+            <h3
+              id={tier.id + tier.name}
+              className="text-base font-semibold leading-7 text-indigo-600"
+            >
+              {tier.name}
+            </h3>
+            <div className="mt-4">
+              {tier.priceMonthly ? (
+                <>
                   <span className="text-5xl font-bold tracking-tight text-gray-900">
-                    Free
+                    {tier.priceMonthly}
                   </span>
-                )}
-                <p className="mt-6 text-bae leading-7 text-gray-600">
-                  {tier.description} 
-                </p>
-                <ul
-                  role="list"
-                  className="mt-10 space-y-4 text-sm leading-6 text-gray-600"
-                >
-                  {tier.feature.map((feature) => (
-                    <li key={feature} className="flex gap-x-3">
-                      <CheckIcon
-                        className="h-6 w-5 flex-none text-indigo-600"
-                        aria-hidden="true"
-                      />
-                      {feature}
-                    </li>
-                  ))}
-                </ul>
-              </div>
+                  <span className="text-base font-semibold leading-7  text-gray-600">
+                    /month
+                  </span>
+                </>
+              ) : (
+                <span className="text-5xl font-bold tracking-tight text-gray-900">
+                  Free
+                </span>
+              )}
+              <p className="mt-6 text-bae leading-7 text-gray-600">
+                {tier.description}
+              </p>
+              <ul
+                role="list"
+                className="mt-10 space-y-4 text-sm leading-6 text-gray-600"
+              >
+                {tier.feature.map((feature) => (
+                  <li key={feature} className="flex gap-x-3">
+                    <CheckIcon
+                      className="h-6 w-5 flex-none text-indigo-600"
+                      aria-hidden="true"
+                    />
+                    {feature}
+                  </li>
+                ))}
+              </ul>
+            </div>
 
-              {redirect ? (
-                <Link
-                  href="/register"
-                  className="mt-8 block rounded-md bg-indigo-600 px-3.5 py-2 text-center text-sm font-semibold leading-6
+            {redirect ? (
+              <Link
+                href="/register"
+                className="mt-8 block rounded-md bg-indigo-600 px-3.5 py-2 text-center text-sm font-semibold leading-6
                 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2
                 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 cursor-pointer
                 disabled:opacity-80"
-                >
-                  Get Started Today
-                </Link>
-              ) : (
-                tier.id && <CheckoutButton />
-              )}
-            </div>
+              >
+                Get Started Today
+              </Link>
+            ) : (
+              tier.id && <CheckoutButton />
+            )}
+          </div>
         ))}
       </div>
     </div>
